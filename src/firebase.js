c30293c2c54bb7acf846b06cdf9543a9f84ff118
Brandon Whitman-Orlin <brandonwhitmanorlin@gmail.com>
2025-12-02 02:08:10 -0500

    Remove console log of Firebase API key
    Eliminated a console.log statement that printed the Firebase API key from environment variables. This improves security by preventing sensitive information from being logged to the console.

--- src/firebase.js
+++ src/firebase.js
@@ -3,8 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-console.log(import.meta.env.VITE_FIREBASE_API_KEY);
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
